3adf5a191ec48d9a32a5a0dbb5324bd46bb30584
sergio-ht <71457700+sergio-ht@users.noreply.github.com>
2020-11-26 14:09:56 -0600

    Add files via upload

---
+++ conexion.py
@@ -0,0 +1,150 @@
+import mysql.connector
+
+bd = mysql.connector.connect(user='sergio',password='12345678',database='cinemapp')
+
+cursor = bd.cursor()
+
+def get_usuarios():
+    consulta = "SELECT * FROM usuario"
+    cursor.execute(consulta)
+    usuarios = []
+    for row in cursor.fetchall():
+        usuario = {
+            'id':row[0],
+            'correo':row[1],
+            'contraseña':row[2]
+        }
+        usuarios.append(usuario)
+    return usuarios
+
+def existe_usuario(correo):
+    query = "SELECT COUNT(*) FROM usuario WHERE correo = %s"
+    cursor.execute(query,(correo,))
+
+    if cursor.fetchone()[0] == 1:
+        return True
+    else:
+        return False
+
+import hashlib
+def crear_usuario(correo,contra):
+    if existe_usuario(correo):
+        return False
+    else:
+        h = hashlib.new('sha256', bytes(contra, 'utf-8'))
+        h = h.hexdigest()
+        insertar = "INSERT INTO usuario(correo, contrasena) VALUES(%s, %s)"
+        cursor.execute(insertar,(correo,h))
+        bd.commit()
+
+        return True
+
+def iniciar_sesion(correo,contra):
+    h = hashlib.new('sha256', bytes(contra, 'utf-8'))
+    h = h.hexdigest()
+    query = "SELECT id FROM usuario WHERE correo = %s AND contrasena = %s"
+    cursor.execute(query,(correo,h))
+    id = cursor.fetchone()
+    if id:
+        return id[0], True
+    else:
+        return None, False
+
+def insertar_pelicula(pelicula):
+    titulo = pelicula['titulo']
+    fecha_visto = pelicula['fecha_visto']
+    imagen = pelicula['imagen']
+    director = pelicula['director']
+    anio = pelicula['anio']
+    usuarioId = pelicula['usuarioId']
+
+    insertar = "INSERT INTO pelicula \
+        (titulo, fecha_visto, imagen, director, anio, usuarioId) \
+        VALUES (%s,%s,%s,%s,%s,%s)"
+    cursor.execute(insertar, (titulo, fecha_visto, imagen, director, anio, usuarioId))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+def get_peliculas():
+    query = "SELECT id, titulo, imagen, fecha_visto, director, anio FROM pelicula"
+    cursor.execute(query)
+    peliculas = []
+    for row in cursor.fetchall():
+        pelicula = {
+            'id' : row[0],
+            'titulo' : row[1],
+            'imagen' : row[2],
+            'fecha_visto' : row[3],
+            'director' : row[4],
+            'anio' : row[5]
+        }
+        peliculas.append(pelicula)
+    return peliculas
+
+def get_pelicula(id):
+    query = "SELECT * FROM pelicula WHERE id = %s"
+    cursor.execute(query,(id,))
+    pelicula = {}
+    row = cursor.fetchone()
+    if row:
+        pelicula['id'] = row[0]
+        pelicula['titulo'] = row[1]
+        pelicula['fecha_visto'] = row[2]
+        pelicula['imagen'] = row[3]
+        pelicula['director'] = row[4]
+        pelicula['anio'] = row[5]
+        pelicula['valoracion'] = row[6]
+        pelicula['favorito'] = row[7]
+        pelicula['resenia'] = row[8]
+        pelicula['compartido'] = row[9]
+
+    return pelicula
+        
+
+def modificar_pelicula(id, columna, valor):
+    update = f"UPDATE pelicula SET {columna} = %s WHERE id = %s"
+    cursor.execute(update,(valor, id))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+    
+
+def eliminar_pelicula(id):
+    eliminar = "DELETE from pelicula WHERE id = %s"
+    cursor.execute(eliminar, (id,))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+
+def get_peliculas_usuario(id):
+    query = "SELECT * FROM pelicula WHERE usuarioId = %s"
+    cursor.execute(query, (id,))
+    peliculas = []
+    for row in cursor.fetchall():
+        pelicula = {
+            'id': row[0],
+            'titulo': row[1],
+            'fecha_visto': row[2],
+            'imagen': row[3],
+            'director': row[4],
+            'anio': row[5],
+            'valoracion': row[6],
+            'favorito': row[7],
+            'resenia': row[8],
+            'compartir': row[9]
+        }
+        peliculas.append(pelicula)
+    return peliculas
+
+
---
+++ main.py
@@ -0,0 +1,82 @@
+from flask import Flask, jsonify, request
+from conexion import crear_usuario, iniciar_sesion
+from conexion import insertar_pelicula, get_peliculas, get_pelicula
+from conexion import modificar_pelicula, eliminar_pelicula
+from conexion import get_peliculas_usuario
+
+app = Flask(__name__)
+
+@app.route("/api/v1/usuarios", methods=["POST"])
+@app.route("/api/v1/usuarios/<int:id>/peliculas", methods=["GET"])
+def usuario(id=None):
+    if request.method == "POST" and request.is_json:
+        try:
+            data = request.get_json()
+            print(data)
+
+            if crear_usuario(data['correo'],data['contraseña']):
+                return jsonify({"code":"ok"})
+            else:
+                return jsonify({"code":"ya_existe"})
+        except:
+            return jsonify({"code":"error"})
+    elif request.method == "GET" and id is not None:
+        return jsonify(get_peliculas_usuario(id))
+
+    
+
+@app.route("/api/v1/sesiones", methods=["POST"])
+def sesion():
+    if request.method == "POST" and request.is_json:
+        try:
+            data = request.get_json()
+            correo = data['correo']
+            contra = data['contraseña']
+            id, ok = iniciar_sesion(correo,contra)
+            if ok:
+                return jsonify({"code":"ok", "id":id})
+            else:
+                return jsonify({"code": "no_existe"})
+        except:
+            return jsonify({"code": "error"})
+
+@app.route("/api/v1/peliculas", methods=["GET", "POST"])
+@app.route("/api/v1/peliculas/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def peliculas(id=None):
+    if request.method == "POST" and request.is_json:
+        try:
+            data = request.get_json()
+            print(data)
+            if insertar_pelicula(data):
+                return jsonify({"code":"ok"})
+            else:
+                return jsonify({"code":"no"})
+        except:
+                return jsonify({"code":"error"})
+
+    elif request.method == "GET" and id is None:
+        return jsonify(get_peliculas())
+    elif request.method == "GET" and id is not None:
+        return jsonify(get_pelicula(id))
+    elif request.method == "PATCH" and id is not None and request.is_json:
+        data = request.get_json()
+        columna = data['columna']
+        valor = data['valor']
+
+        if modificar_pelicula(id, columna, valor):
+            return jsonify(code='ok')
+        else:
+            return jsonify(code='error')
+    elif request.method == "DELETE" and id is not None:
+        if eliminar_pelicula(id):
+            return jsonify(code='ok')
+        else:
+            return jsonify(code='error')
+
+
+
+
+
+
+
+app.run(debug=True)
